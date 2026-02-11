bfb06191f602db50460cc3263a8ec779308fb405
ahmedismail-10 <ahmedismaeel064@gmail.com>
2026-02-11 15:04:14 +0200

    Add FontFamily

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:responsive_dash_board/views/dash_board_view.dart';
 
 void main() {
@@ -10,9 +11,12 @@ class ResponsiveDashBoard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(),
+      ),
       debugShowCheckedModeBanner: false,
-      home: DashBoardView(),
+      home: const DashBoardView(),
     );
   }
 }
