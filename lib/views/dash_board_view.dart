a2129784ea87571f1c1d8054f07e7dc132a07e27
Ahmed Ismail <ahmedismaeel064@gmail.com>
2026-09-02 08:34:23 +0300

    feat: implement DashBoardDesktopLayout and integrate it into DashBoardView using AdaptiveLayout

--- lib/views/dash_board_view.dart
+++ lib/views/dash_board_view.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/adaptive_layout.dart';
+import 'package:responsive_dash_board/widgets/dash_board_desktop_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const new({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return AdaptiveLayout(
+      mobileLayout: (context) => const SizedBox(),
+      tabletLayout: (context) => const SizedBox(),
+      desktopLayout: (context) => const DashBoardDesktopLayout(),
+    );
   }
 }
