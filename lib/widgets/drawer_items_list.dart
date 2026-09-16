bd364808dd462a21a1b296d1db15dfb53d866c37
Ahmed Ismail <ahmedismaeel064@gmail.com>
2026-09-16 15:21:56 +0300

    Create Active&InActive DrawerItem

--- lib/widgets/drawer_items_list.dart
+++ lib/widgets/drawer_items_list.dart
@@ -3,9 +3,14 @@ import 'package:responsive_dash_board/models/drawer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/drawer_item.dart';
 
-class DrawerItemsList extends StatelessWidget {
+class DrawerItemsList extends StatefulWidget {
   const new({super.key});
 
+  @override
+  State<DrawerItemsList> createState() => _DrawerItemsListState();
+}
+
+class _DrawerItemsListState extends State<DrawerItemsList> {
   final List<DrawerItemModel> items = const [
     DrawerItemModel(image: Assets.imagesDashboard, title: 'Dashboard'),
     DrawerItemModel(image: Assets.imagesMyTransctions, title: 'My Transaction'),
@@ -14,6 +19,8 @@ class DrawerItemsList extends StatelessWidget {
     DrawerItemModel(image: Assets.imagesMyInvestments, title: 'My Investments'),
   ];
 
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -21,9 +28,21 @@ class DrawerItemsList extends StatelessWidget {
       physics: const NeverScrollableScrollPhysics(),
       itemCount: items.length,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const .only(top: 20),
-          child: DrawerItem(drawerItemModel: items[index]),
+        return GestureDetector(
+          onTap: () {
+            if (index != selectedIndex) {
+              setState(() {
+                selectedIndex = index;
+              });
+            }
+          },
+          child: Padding(
+            padding: const .only(top: 20),
+            child: DrawerItem(
+              drawerItemModel: items[index],
+              isActive: (selectedIndex == index),
+            ),
+          ),
         );
       },
     );
