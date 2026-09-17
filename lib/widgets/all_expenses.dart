2e0fbb033bf487842de9223630c7b1cbc3a13003
Ahmed Ismail <ahmedismaeel064@gmail.com>
2026-09-17 16:16:09 +0300

    Create AllExpensess widget

---
+++ lib/widgets/all_expenses.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AllExpenses extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const .all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: .circular(12),
+      ),
+      child: const Column(children: []),
+    );
+  }
+}
