a3adf7f7948a5a52800ad717644c8321fc09d275
Ahmed Ismail <ahmedismaeel064@gmail.com>
2026-09-19 18:09:23 +0300

    Craete AllExpensesItemsListView

--- lib/widgets/all_expenses.dart
+++ lib/widgets/all_expenses.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/models/all_expenses_item_model.dart';
-import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_header.dart';
-import 'package:responsive_dash_board/widgets/all_expenses_item.dart';
+
+import 'package:responsive_dash_board/widgets/all_expenses_items_list_view.dart';
 
 class AllExpenses extends StatelessWidget {
   const new({super.key});
@@ -21,14 +20,7 @@ class AllExpenses extends StatelessWidget {
           SizedBox(
             height: 16,
           ),
-          AllExpensesItem(
-            itemModel: AllExpensesItemModel(
-              image: Assets.imagesIncome,
-              title: 'Income',
-              date: 'April 2022',
-              price: '\$20,129',
-            ),
-          ),
+          AllExpensesItemsListView(),
         ],
       ),
     );
---
+++ lib/widgets/all_expenses_items_list_view.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/all_expenses_item_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/all_expenses_item.dart';
+
+class AllExpensesItemsListView extends StatelessWidget {
+  const new({super.key});
+
+  final List<AllExpensesItemModel> items = const [
+    AllExpensesItemModel(
+      image: Assets.imagesBalance,
+      title: 'Balance',
+      date: 'April 2022',
+      price: '\$20,129',
+    ),
+    AllExpensesItemModel(
+      image: Assets.imagesIncome,
+      title: 'Income',
+      date: 'April 2022',
+      price: '\$20,129',
+    ),
+    AllExpensesItemModel(
+      image: Assets.imagesExpenses,
+      title: 'Expenses',
+      date: 'April 2022',
+      price: '\$20,129',
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: 12,
+      children: items
+          .map(
+            (e) => Expanded(child: AllExpensesItem(itemModel: e)),
+          )
+          .toList(),
+    );
+  }
+}
