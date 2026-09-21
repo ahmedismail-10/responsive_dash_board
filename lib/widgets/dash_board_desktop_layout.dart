f04357297a346a8954f0a33217373aac0c70660b
Ahmed Ismail <ahmedismaeel064@gmail.com>
2026-09-21 20:09:51 +0300

    Create QuickInvoice Widget

--- lib/widgets/dash_board_desktop_layout.dart
+++ lib/widgets/dash_board_desktop_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/widgets/all_expenses.dart';
 import 'package:responsive_dash_board/widgets/custom_drawer.dart';
+import 'package:responsive_dash_board/widgets/quick_invoice.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const new({super.key});
@@ -17,9 +18,9 @@ class DashBoardDesktopLayout extends StatelessWidget {
           flex: 2,
           child: Column(
             children: [
-              Expanded(
-                child: AllExpenses(),
-              ),
+              AllExpenses(),
+              SizedBox(height: 24),
+              QuickInvoice(),
             ],
           ),
         ),
---
+++ lib/widgets/quick_invoice.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/widgets/custom_background_container.dart';
+
+class QuickInvoice extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomBackgoundContainer(
+      child: Column(),
+    );
+  }
+}
